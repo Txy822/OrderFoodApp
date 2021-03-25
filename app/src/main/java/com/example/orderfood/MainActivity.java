8fd409c3a7668b9e8bac6a3e4f063f7398d9a535
tyohanis-wolleli <tyohanis2018@gmail.com>
2021-03-25 22:04:27 +0000

    methods  added

--- app/src/main/java/com/example/orderfood/MainActivity.java
+++ app/src/main/java/com/example/orderfood/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void order(){
+
+    }
+    public void special(){
+
+    }
+    public void recommended(){
+
+    }
 }
